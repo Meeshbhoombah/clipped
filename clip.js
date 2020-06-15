f4bbca0c1dd0b25b4c860e401dd64661a9631182
Rohan Mishra <rohanmishra2017@gmail.com>
2020-06-14 23:37:39 -0700

    Started CLI layout

--- clip.js
+++ clip.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
 'use strict';
 
-console.log("Test");
+const cli = require('yargs');
+
+
+/***** default *****/
+if (cli.argv._[0] == null) {
+    cli.showHelp();
+/***** <profile-url> *****/
+} else {
+    console.log(cli.argv._);
+}
+
+/***** -h, --help *****/
+cli
+    .usage('Usage: $0 <command> [options]')
+    .help('h')
+    .alias('h', 'help').argv
+
+/***** init (-wt, --with-token <token>) *****/
+
+/***** browse (<user>) *****/
 
